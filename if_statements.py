8b6257be877dbe7285d123f24bece863631daf0a
arsh <arshlegend14@gmail.com>
2024-09-30 20:08:54 +0530

    added code to demonstrate the use of a modulus operator.

---
+++ if_statements.py
@@ -0,0 +1,33 @@
+
+
+
+# print("type of books")
+# book = input()
+#
+# if book == "adventure":
+#     print("I like adventure books")
+#
+# print("Finished reading book")
+
+
+# print("enter an activity")
+# calculate=input()
+# if calculate == "calculate":
+#     print("Performing caluculations")
+# else :
+#     print("performing activity ")
+# print("activity completed")
+
+
+print("towards which direction should i go ")
+Robot=input()
+if Robot == "up":
+    print("i am moving in upward direction")
+elif Robot == "down":
+    print("i am moving in downward direction")
+elif Robot == "left":
+    print("i am moving to left direction")
+elif Robot == "right":
+    print(" i am moving to right direction")
+
+
--- output_tasks.py
+++ output_tasks.py
@@ -1,5 +1,7 @@
-print("welcome to COM411!")
+from linecache import clearcache
 
+print("welcome to COM411!")
+clearcache()
 
 
 
